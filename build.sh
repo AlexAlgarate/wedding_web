a559f2c32bb92fd1dc9563bc91cc81067bc0328a
Alex <alex.algarate@gmail.com>
2024-01-26 08:25:37 +0100

    actions

--- build.sh
+++ build.sh
@@ -1,13 +1,15 @@
-source venv/Scripts/activate
+cd python_web
+python -m venv .venv
+source .venv/Scripts/activate
 python.exe -m pip install --upgrade pip
 pip install -r requirements.txt
+rm -fr public
 reflex init
 reflex export --frontend-only
-rm -fr public
 unzip frontend.zip -d public
 rm -f frontend.zip
 echo ".web/" > .gitignore
 echo "__pycache__/" >> .gitignore
-echo "venv/" >> .gitignore
+echo ".venv/" >> .gitignore
 echo ".env" >> .gitignore
 deactivate
