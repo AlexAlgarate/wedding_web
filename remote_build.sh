ebd18555c47db4f77e441a684ad82f94d962331c
Alex <alex.algarate@gmail.com>
2024-01-27 07:26:13 +0100

    env variables GAction

--- remote_build.sh
+++ remote_build.sh
@@ -1,6 +1,8 @@
 ls
-# python -m venv .venv
-# source .venv/bin/activate
+echo "VICKY_PHONE: $VICKY_PHONE"
+echo "VICKY_EMAIL: $VICKY_EMAIL"
+echo "ALEX_PHONE: $ALEX_PHONE"
+echo "ALEX_EMAIL: $ALEX_EMAIL"
 python -m pip install --upgrade pip
 pip install -r requirements.txt
 rm -fr public
@@ -8,4 +10,3 @@ reflex init
 reflex export --frontend-only
 unzip frontend.zip -d public
 rm -f frontend.zip
-# deactivate
