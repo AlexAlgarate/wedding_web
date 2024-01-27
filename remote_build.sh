405a3516b990110eeed765bcafdc81d31d75cf73
Alex <alex.algarate@gmail.com>
2024-01-27 07:29:33 +0100

    WIP

--- remote_build.sh
+++ remote_build.sh
@@ -1,9 +1,6 @@
 ls
 echo "ESTAS SON LAS VARIABLES DE ENTORNO"
 echo "VICKY_PHONE: $VICKY_PHONE"
-echo "VICKY_EMAIL: $VICKY_EMAIL"
-echo "ALEX_PHONE: $ALEX_PHONE"
-echo "ALEX_EMAIL: $ALEX_EMAIL"
 python -m pip install --upgrade pip
 pip install -r requirements.txt
 rm -fr public
