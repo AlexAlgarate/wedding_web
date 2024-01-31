0c9d707f1892a8bc364d1f2af460e757221e12a7
AlexAlgarate <alex.algarate@gmail.com>
2024-01-31 06:44:55 +0000

    Update static build for Vercel

---
+++ public/js/countdown.js
@@ -0,0 +1,55 @@
+// var countDownDate = new Date("Aug 30, 2024 18:00:00 UTC").getTime();
+
+// var x = setInterval(function () {
+//     var countdownElement = document.getElementById("countdown");
+
+//     if (countdownElement !== null) {
+//         var now = new Date().getTime();
+//         var distance = countDownDate - now;
+
+//         var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+//         var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+//         var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+//         var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+//         countdownElement.innerHTML = days + "d - " + hours + "h - " + minutes + "m - " + seconds + "s  ";
+
+//         if (distance < 0) {
+//             clearInterval(x);
+//             countdownElement.innerHTML = "¡Comienza nuestra boda!";
+//         }
+//     }
+// }, 1000);
+
+
+var countDownDate = new Date("Aug 30, 2024 18:00:00 UTC").getTime();
+
+var x = setInterval(function () {
+    var daysElement = document.getElementById("days");
+    var hoursElement = document.getElementById("hours");
+    var minutesElement = document.getElementById("minutes");
+    var secondsElement = document.getElementById("seconds");
+
+    if (daysElement !== null && hoursElement !== null && minutesElement !== null && secondsElement !== null) {
+        var now = new Date().getTime();
+        var distance = countDownDate - now;
+
+        var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+        var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+        var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+        daysElement.innerHTML = days ;
+        hoursElement.innerHTML = hours ;
+        minutesElement.innerHTML = minutes ;
+        secondsElement.innerHTML = seconds ;
+
+        if (distance < 0) {
+            clearInterval(x);
+            daysElement.innerHTML = "¡Comienza nuestra boda!";
+            hoursElement.innerHTML = "";
+            minutesElement.innerHTML = "";
+            secondsElement.innerHTML = "";
+        }
+    }
+}, 1000);
