88d0083acf7c985a972f1bf94db2485c80808abf
Alex <alex.algarate@gmail.com>
2024-10-03 00:28:26 +0200

    Update countdown to don't breake UI

--- wedding/views/countdown/countdown.py
+++ wedding/views/countdown/countdown.py
@@ -3,10 +3,30 @@ import reflex as rx
 import wedding.urls as url
 from wedding import utils as utils
 from wedding.components import secondary_button, text_section
-from wedding.routes import FileRoutes
-from wedding.styles import Color
+from wedding.styles import Color, Font, FontHeight, FontWeight, Size
 
 from .components import create_countdown_vstack
+
+
+def finished_wedding(text: str) -> rx.Component:
+    return rx.text(
+        text,
+        color=Color.NUMBERS_COUNTDOWN.value,
+        width="100%",
+        height="100%",
+        font_family=Font.TITLE.value,
+        text_align="center",
+        font_style="normal",
+        font_weight=FontWeight.MEDIUM_INSIDE_TEXTS.value,
+        line_height=FontHeight.NORMAL.value,
+        font_size=[
+            Size.BIG.value,
+            Size.BIG.value,
+            Size.BIG.value,
+            Size.BIG.value,
+            Size.BIG.value,
+        ],
+    )
 
 
 def countdown() -> rx.Component:
@@ -16,18 +36,22 @@ def countdown() -> rx.Component:
     Returns:
         rx.Component: The countdown component.
     """
-    countdown_elements = [
-        create_countdown_vstack(element="days", text_date="Días"),
-        create_countdown_vstack(element="hours", text_date="horas"),
-        create_countdown_vstack(element="minutes", text_date="mins"),
-        create_countdown_vstack(element="seconds", text_date="segs"),
-    ]
+
+    # As the wedding has already celebrated, this lines breake the UI.
+    # countdown_elements = [
+    #     create_countdown_vstack(element="days", text_date="Days"),
+    #     create_countdown_vstack(element="hours", text_date=""),
+    #     create_countdown_vstack(element="minutes", text_date=""),
+    #     create_countdown_vstack(element="seconds", text_date=""),
+    # ]
     return rx.vstack(
         text_section(utils.countdown_text),
         secondary_button(utils.countdown_button, url=url.CALENDAR_HTML),
         rx.flex(
-            *countdown_elements,
-            rx.script(src=FileRoutes.JS_COUNTDOWN.value),
+            # As the wedding has already celebrated, this lines breake the UI.
+            # *countdown_elements,
+            # rx.script(src=FileRoutes.JS_COUNTDOWN.value),
+            finished_wedding("¡Ya hemos celebrado nuestra boda!"),
             justify="center",
             align="center",
             padding="16px 0px",
